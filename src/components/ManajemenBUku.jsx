793e3bd5ee0096a3376a0f495847d3e4866f4b8a
Nayandra <kastoronayandra@gmail.com>
2023-06-02 14:05:08 +0700

    first commit

---
+++ src/components/ManajemenBUku.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ManajemenBuku = () => {
+  return (
+      <div className="container mt-3">
+         <h1 className="tetxt-center">Manajemen Buku</h1>
+         <button className="btn btn-sm btn-primary my-2">
+
+         </button>
+
+      </div>
+  );
+};
+
+export default ManajemenBuku;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from './../assets/camp404v2.png';
+
+const Navbar = () => {
+   return (
+      <nav className="navbar navbar-expand-lg bg-light shadow">
+         <div className="container">
+            <ul className="nav">
+               <img src={logo} alt="" style={{height: "40px"}}/>
+               <li>
+                  <a href="/" className="nav-link">Beranda</a>
+               </li>
+               <li>
+                  <a href="/manajemen" className="nav-link">
+                     Manajemen Buku
+                  </a>
+               </li>
+            </ul>
+
+         </div>
+      </nav>
+   )
+};
+
+export default Navbar;
